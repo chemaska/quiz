913d71b8bd22753328d8fa7f7fee11786ee61526
chemaska <chemaska@gmail.com>
2015-07-17 17:09:49 -0600

    Ejercicio P2P
    Esta es una muestra de tarea

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,3 +11,7 @@ exports.answer = function(req, res){
 		res.render('quizes/answer',{respuesta: 'Incorrecta'});
 	}
 };
+
+exports.author = function(req, res){
+	res.render('author',{nombre: 'Alejandro Chavarría'});
+};
